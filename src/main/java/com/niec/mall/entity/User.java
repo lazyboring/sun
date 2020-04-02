bdcc80e5e814e18e29206a1d6762ca3d76bdb7c5
nc <1007749243@qq.com>
2020-04-02 20:38:24 +0800

    yyu

--- src/main/java/com/niec/mall/entity/User.java
+++ src/main/java/com/niec/mall/entity/User.java
@@ -1,5 +1,6 @@
 package com.niec.mall.entity;
 
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import lombok.Data;
 
 import java.util.Date;
@@ -15,31 +16,34 @@ import java.io.Serializable;
 public class User implements Serializable {
     private static final long serialVersionUID = 394144134152575660L;
     /**
-    * 主键
-    */
+     * 主键
+     */
     private Long id;
     /**
-    * 姓名
-    */
+     * 姓名
+     */
     private String name;
     /**
-    * 年龄
-    */
+     * 年龄
+     */
     private Integer age;
     /**
-    * 邮箱
-    */
+     * 邮箱
+     */
     private String email;
     /**
-    * 直属上级id
-    */
+     * 直属上级id
+     */
     private Long managerId;
     /**
-    * 创建时间
-    */
+     * 创建时间
+     */
     private Date createTime;
 
-
-
+    /**
+     *逻辑删除
+     */
+    @TableLogic
+    private Integer deleted;
 
 }
